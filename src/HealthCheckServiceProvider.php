04c6202d68f7ac391cb85231a8d9057825a5e193
Vasilyev Vadim <v.vasilyev@growfood.pro>
2021-05-19 11:32:01 +0300

    fix

--- src/HealthCheckServiceProvider.php
+++ src/HealthCheckServiceProvider.php
@@ -2,37 +2,19 @@
 
 namespace Gftech\Healthcheck;
 
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
 
 class HealthCheckServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap services.
-     *
-     * @return void
-     */
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__ . '/../routes/api.php');
-
         $this->publishes([
-            __DIR__ . '/../config/healthcheck.php' => config_path('healthcheck.php'),
-        ]);
-
-        $this->mergeConfigFrom(
-            __DIR__ . '/../config/healthcheck.php', 'healthcheck'
-        );
-
-        $this->loadMigrationsFrom(__DIR__ . '/../migrations');
-    }
-
-    /**
-     * Register services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        $this->app->make(HealthCheckController::class);
+            __DIR__ . '/../config/healthcheck.php' => App::configPath('healthcheck.php'),
+        ], 'config');
+        $this->publishes([
+            __DIR__ . '/../migrations' => App::databasePath('migrations'),
+        ], 'migrations');
+        $this->loadRoutesFrom(__DIR__ . '/../routes.php');
     }
 }
